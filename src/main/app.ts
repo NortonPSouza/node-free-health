782ac6ef60fd7901c093853703f500b31bd5cdf1
Norton Souza <nortonperantoni@gmail.com>
2022-07-06 16:14:21 -0300

    initial: first commit :sparkles:

---
+++ src/main/app.ts
@@ -0,0 +1,19 @@
+import express, { Express } from 'express';
+
+export class Application {
+
+    private app: Express;
+    private readonly PORT = 3000;
+
+    constructor() {
+        this.app = express();
+        this.server();
+    }
+
+    private server(): void {
+        this.app.listen(this.PORT, () => {
+            console.log(`⚡️[server]: Server is running at https://localhost:${this.PORT}`);
+        });
+    }
+
+}
